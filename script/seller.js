8833d77ceef87af6227b153485a9ffbebba15ec4
Ibsa Abraham <62568756+ibsa21@users.noreply.github.com>
2022-02-25 19:03:07 +0300

    update change_tab() function
    made sure appropriate tab will be displayed

--- script/seller.js
+++ script/seller.js
@@ -1,4 +1,4 @@
-
+//select 
 const shrinkBtn = document.querySelector(".shrink-btn");
 const search = document.querySelector(".search");
 const sidebar_links = document.querySelectorAll(".sidebar-links a");
@@ -11,13 +11,14 @@ const sidebar = document.getElementById('sidebar')
 const body = document.querySelector('b')
 const main = document.querySelector('a')
 
-
+// select tab elements
 const dashboard = document.getElementById('zero');
 const product_container = document.getElementById('one');
 const order_info = document.getElementById('two');
 const add_product = document.getElementById('three');
 const add_product_link = document.getElementById('add')
 
+//navigation menu clicked toggle between display flex and none
 menu.addEventListener('click', () => {
   if (sidebar.style.display == "flex") {
     sidebar.style.display = "none";
@@ -28,6 +29,7 @@ menu.addEventListener('click', () => {
 
 })
 
+// add screen-overlay feature
 body.addEventListener('click', () => {
   body.classList.remove('screen-overlay', 'show')
   sidebar.style.display = "none";
@@ -35,6 +37,7 @@ body.addEventListener('click', () => {
 let activeIndex;
 
 
+// shrink sidebar navigation 
 shrinkBtn.addEventListener("click", () => {
 
   document.body.classList.toggle("shrink");
@@ -44,19 +47,20 @@ shrinkBtn.addEventListener("click", () => {
   setTimeout(() => shrinkBtn.classList.remove("hovered"), 500);
 });
 
+//make sidebar bigger again upon click of search button
 search.addEventListener("click", () => {
 
   document.body.classList.remove("shrink");
   search.lastElementChild.focus();
 });
 
-
+// apply style to sidebar tab based on it's distance from top
 function style_activeTab() {
   let from_top = activeIndex * 58 + 2.5;
   active_tab.style.top = `${from_top}px`;
 }
 
-
+// change active tab style
 function changeLink() {
 
   sidebar_links.forEach((sideLink) => sideLink.classList.remove("active"));
@@ -92,6 +96,7 @@ list_elements.forEach((elem) => {
   elem.addEventListener("mouseover", showListStyle);
 });
 
+// toggle between tabs
 function change_tab(old_tab, activeIndex) {
   console.log("hello")
   if (activeIndex != old_tab) {
